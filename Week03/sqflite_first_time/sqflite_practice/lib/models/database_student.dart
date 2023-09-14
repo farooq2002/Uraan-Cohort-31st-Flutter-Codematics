9ebc20448e1825e05ea8b7b998001a785ae9471d
Farooq2002 <fa552991@gmail.com>
2023-09-14 23:31:10 +0500

    sqflite-comoplte

--- Week03/sqflite_first_time/sqflite_practice/lib/models/database_student.dart
+++ Week03/sqflite_first_time/sqflite_practice/lib/models/database_student.dart
@@ -1,5 +1,6 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart' as Path;
+import 'package:sqflite_practice/models/model_student.dart';
 
 const String databaseName = "student_db";
 const String studentTable = "student_table";
@@ -7,42 +8,143 @@ const String columnId = "column_id";
 const String columnName = "column_name";
 
 class StudentDatabase {
+  // Static variable to hold the reference to the SQLite database.
   static Database? _database;
 
   Future<Database> get database async {
-    //if _database==null assign initializedDatabase to
-    _database ??= await initializedDatabase();
-    return database;
+    if (_database == null) {
+      _database = await initializedDatabase();
+    }
+
+    return _database!;
   }
 
+  // Static variable to hold a single instance of the StudentDatabase class.
   static StudentDatabase? _studentDatabase;
+  // Private constructor to ensure that instances are only created within the class.
   StudentDatabase._onCreateInstance();
+// Factory constructor for creating instances of the StudentDatabase class.
+  factory StudentDatabase() {
+    if (_studentDatabase == null) {
+      _studentDatabase = StudentDatabase._onCreateInstance();
+    }
+    return _studentDatabase!;
+  }
 
-  factory StudentDatabase() =>
-      // (??=) a shorthand way to assign a value to a variable only if that variable
-      // is currently null.
-      _studentDatabase ??= StudentDatabase._onCreateInstance();
-
-  Future<Database> initializedDatabase() async {
+  Future<Database?> initializedDatabase() async {
     try {
+      // Get the path to the directory where databases are stored.
       var databasPath = await getDatabasesPath();
+      // Join the database name with the directory path to create the full path.
       String path = Path.join(databasPath, databaseName);
 
+      // Open the database, specifying the schema to create the student table.
       Database database = await openDatabase(
         path,
-        version: 1,
+        version: 2,
         onCreate: (Database db, int version) {
+          // Create the student table with columns: column_id (integer, primary key)
+          // and column_name (text).
           db.execute('''
-          create table $studentTable(
-          $columnId INTEGER PRIMARY KEY,
-          $columnName text not null)
+            CREATE TABLE $studentTable (
+      $columnId INTEGER PRIMARY KEY,
+      $columnName TEXT NOT NULL
+    )
         ''');
+        },
+        onUpgrade: (Database db, int oldVersion, int newVersion) {
+          if (oldVersion < 2) {
+            // Perform your schema migration here if needed
+          }
         },
       );
       return database;
     } catch (e) {
-      print(e.toString());
-      return null!;
+      print("Database error : ${e.toString()}");
+      return null;
+    }
+  }
+
+  //============ Method for adding new student to the database=====================================
+  Future<bool> addStudent(ModelStudent modelStudent) async {
+    try {
+      Database? db = await this.database;
+      var result = await db.insert(studentTable, modelStudent.toMap());
+      print("Student added database : $result");
+      return true;
+    } catch (e) {
+      print("there is some Student added database error : ${e.toString()}");
+      return false;
+    }
+  }
+
+  //======== Method for updating a specific student data in database ===========================
+  Future<bool> updateStudent(ModelStudent modelStudent) async {
+    try {
+      Database db = await this.database;
+      var result = await db.update(studentTable, modelStudent.toMap(),
+          where: "$columnId=?", whereArgs: [columnId]);
+      return true;
+    } catch (e) {
+      print("Student updated database error : ${e.toString()}");
+      return false;
+    }
+  }
+
+  //===== Method for deleting Deleting specific student data from database tables by id ============
+  Future<bool> deleteStudent(String id) async {
+    try {
+      Database db = await this.database;
+      var result =
+          await db.delete(studentTable, where: "$columnId=?", whereArgs: [id]);
+      return true;
+    } catch (e) {
+      print("Student deleted database error : ${e.toString()}");
+      return false;
+    }
+  }
+
+  // Delete all students from database
+  Future<bool> deleteAllStudent(String id) async {
+    try {
+      Database db = await this.database;
+      var result = await db.delete(studentTable);
+      return true;
+    } catch (e) {
+      print("Student deleted database error : ${e.toString()}");
+      return false;
+    }
+  }
+
+  //=========== Get all students data =========================================
+  Future<List<ModelStudent>> getAllStudent(ModelStudent modelStudent) async {
+    List<ModelStudent> list_student = [];
+    try {
+      Database db = await this.database;
+      List<Map<String, dynamic>> result =
+          await db.query(studentTable, orderBy: "$columnId DESC");
+      if (result.isNotEmpty) {
+        for (int i = 0; i < result.length; i++) {
+          ModelStudent modelStudent = ModelStudent.fromMap(result[i]);
+          list_student.add(modelStudent);
+        }
+      }
+      return list_student;
+    } catch (e) {
+      print("Student getAllStudent database error : ${e.toString()}");
+      return list_student;
+    }
+  }
+
+  //get count of the student table
+  Future<int> getCountStudent() async {
+    try {
+      Database db = await this.database;
+      List<Map<String, dynamic>> result = await db.query(studentTable);
+      return result.length;
+    } catch (e) {
+      print("Get count database error : ${e.toString()}");
+      return 0;
     }
   }
 }
--- Week03/sqflite_first_time/sqflite_practice/lib/screens/first_screen.dart
+++ Week03/sqflite_first_time/sqflite_practice/lib/screens/first_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite_practice/models/database_student.dart';
+import 'package:sqflite_practice/models/model_student.dart';
 
 class FirstScreen extends StatefulWidget {
   const FirstScreen({super.key});
@@ -8,10 +10,155 @@ class FirstScreen extends StatefulWidget {
 }
 
 class _FirstScreenState extends State<FirstScreen> {
+  String id = "";
+  String name = "";
+  StudentDatabase databaseStudent = StudentDatabase();
+
+  @override
+  void initState() {
+    super.initState();
+
+    ModelStudent ss = ModelStudent(id: 1, name: 'Farooq ahmad');
+    ModelStudent object = ModelStudent.fromMap(ss.toMap());
+
+    setState(() {
+      databaseStudent.initializedDatabase();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: const Text("SQFLITE")),
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        centerTitle: true,
+        title: const Text(
+          "SQFLITE PRACTICE",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.black)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 7, vertical: 5),
+                child: TextField(
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.phone_android),
+                      hintText: "Type Id here",
+                      border: InputBorder.none),
+                  onChanged: (value) {
+                    setState(() {
+                      id = value;
+                      print(id);
+                    });
+                  },
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.black)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 7, vertical: 5),
+                child: TextField(
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.person_2),
+                      hintText: "Type name here",
+                      border: InputBorder.none),
+                  onChanged: (value) {
+                    name = value;
+                    print(name);
+                  },
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        ModelStudent modelStudent =
+                            ModelStudent(id: int.parse(id), name: name);
+
+                        databaseStudent
+                            .updateStudent(modelStudent)
+                            .then((value) {
+                          if (value) {
+                            print("Record updated Successfully!");
+                          } else {
+                            print("Record updated failed!");
+                          }
+                        });
+                      },
+                      child: const Text("Update Student")),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        ModelStudent modelStudent =
+                            ModelStudent(id: int.parse(id), name: name);
+
+                        databaseStudent.addStudent(modelStudent).then((value) {
+                          if (value) {
+                            print("Student added sucessfully!");
+                          } else {
+                            print("Student data failed!");
+                          }
+                        });
+                      },
+                      child: const Text("Add Student"))
+                ],
+              ),
+            ),
+            // Padding(
+            //   padding: const EdgeInsets.all(8.0),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.center,
+            //     children: [
+            //       ElevatedButton(
+            //           onPressed: () {}, child: const Text("Add Student")),
+            //       const SizedBox(
+            //         width: 10,
+            //       ),
+            //       ElevatedButton(
+            //           onPressed: () {}, child: const Text("Add Student"))
+            //     ],
+            //   ),
+            // ),
+            // Padding(
+            //   padding: const EdgeInsets.all(8.0),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.center,
+            //     children: [
+            //       ElevatedButton(
+            //           onPressed: () {}, child: const Text("Add Student")),
+            //       const SizedBox(
+            //         width: 10,
+            //       ),
+            //       ElevatedButton(
+            //           onPressed: () {}, child: const Text("Add Student"))
+            //     ],
+            //   ),
+            // )
+          ],
+        ),
+      ),
     );
   }
 }
